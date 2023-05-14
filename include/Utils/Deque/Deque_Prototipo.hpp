5453bfcdf20ea58eafa53aa48cefb82c50b758db
JoelJimenez23 <joeljc2004@hotmail.com>
2023-05-14 02:25:03 -0500

    new deque prototype version

--- include/Utils/Deque/Deque_Prototipo.hpp
+++ include/Utils/Deque/Deque_Prototipo.hpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int chunksize = 8;
 
+int chunksize = 3;
+
+//buff_size is the length of the chunk
 template<typename T>
 struct deque_iterator{
     typedef deque_iterator iterator;
@@ -27,10 +30,51 @@ struct deque_iterator{
         }
         return *this;
     }
-    T& operator[](size_t n)const{
-        return *(*this + n);
+    iterator& operator--(){
+        if(current == first){
+            set_node(node -1);
+            current = last;
+        }
+        --current;
+        return *this;
     }
 
+    iterator& operator+=(ptrdiff_t n){
+        ptrdiff_t  offset = n + (current - first);
+        if(offset >= 0 && offset< ptrdiff_t(chunksize)){
+            current += n;
+        }
+        else{
+            ptrdiff_t node_offset;
+            if(offset > 0){
+                node_offset = offset / ptrdiff_t(chunksize);
+            }
+            else{
+                node_offset = -((offset - 1) / ptrdiff_t(chunksize)) - 1;
+            }
+            set_node(node + node_offset);
+            current = first + (offset - node_offset * chunksize);
+        }
+        return *this;
+    }
+    iterator operator+(ptrdiff_t n)const{
+        iterator temp = *this;
+        return temp += n;
+    }
+    iterator& operator -=(ptrdiff_t n)const{
+        iterator tmp = *this;
+        return tmp += n;
+    }
+    iterator operator-(ptrdiff_t n)const{
+        iterator tmp = *this;
+        return tmp -= n;
+    }
+    T& operator[](ptrdiff_t n)const{
+        return *(*this + n);
+    }
+    bool operator!=(const iterator& other){
+        return this->current != other.current;
+    }
 };
 
 template<typename T>
@@ -52,50 +96,69 @@ private:
             ++num_nodes;
         }
         map_size = size_t(std::max(8,int(num_nodes + 2)));
-        map = mapAllocator::allocate(map_size);
-
-        T** tmp_start = map + (map_size - num_nodes) / 2;
-        T** tmp_finish = tmp_start + num_nodes - 1;
-        T** cur;
-
-        for(T** current = tmp_start;current <= tmp_finish;++current){
-            *current = dataAllocator::allocate(chunksize);
-        }
-
-
-        start.set_node(tmp_start);
-        start.current = start.first;
-
-        finish.set_node(tmp_finish);
-        finish.current = finish.first + num_elements % chunksize;
-    }
-
-public:
-    deque()=default;
-    explicit deque(size_t num_elements){
-        size_t num_nodes = num_elements / chunksize;
-        map_size = size_t(std::max(8,int(num_nodes + 2)));
-        mapAllocator alloc;
+        mapAllocator  alloc;
         map = alloc.allocate(map_size);
 
         T** tmp_start = map + (map_size - num_nodes) / 2;
         T** tmp_finish = tmp_start + num_nodes - 1;
-
+        T** cur;
         dataAllocator alloc1;
         for(T** current = tmp_start;current <= tmp_finish;++current){
             *current = alloc1.allocate(chunksize);
         }
 
+
         start.set_node(tmp_start);
         start.current = start.first;
 
         finish.set_node(tmp_finish);
         finish.current = finish.first + num_elements % chunksize;
+    }
 
+    void fill_initialize(size_t n,const T& value){
+        create_map_and_nodes(n);
 
-    };
+        T* cur;
+        for(cur = start.current; cur != finish.current; ++cur){
+            *cur = value;
+        }
+    }
+
+public:
+    deque()=default;
+    explicit deque(size_t num_elements){
+        fill_initialize(num_elements,T());
+    }
+    deque(size_t num_elements,const T& value){
+        fill_initialize(num_elements,value);
+    }
+    T& front(){
+        return *start;
+    }
+    T& back(){
+        iterator tmp = finish;
+        --tmp;
+        return *tmp;
+    }
     T& operator[](size_t n){
         return start[n];
     }
-
+    void push_back(const T& value){
+        *(finish.current) = value;
+        ++finish.current;
+    }
+    void push_front(const T& value){
+        *(start.current) = value;
+        --start.current;
+    }
+    T& set_front(){
+        iterator tmp = start;
+        ++tmp;
+        return *tmp;
+    }
+    void test(){
+        --start.current;
+    }
+    iterator begin(){return start;}
+    iterator end(){return finish;}
 };
